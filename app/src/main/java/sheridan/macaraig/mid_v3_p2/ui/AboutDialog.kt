fa102510139eb6ed4ce644d86039bd50b26b073e
Ryle Macaraig <macaraig@sheridancollege.ca>
2020-10-21 14:53:30 -0400

    completed about dialog and debugged errors. Code is running well with no issues

--- app/src/main/java/sheridan/macaraig/mid_v3_p2/ui/AboutDialog.kt
+++ app/src/main/java/sheridan/macaraig/mid_v3_p2/ui/AboutDialog.kt
@@ -1,4 +1,18 @@
 package sheridan.macaraig.mid_v3_p2.ui
 
-class AboutDialog {
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import sheridan.macaraig.mid_v3_p2.R
+
+class AboutDialog : DialogFragment(){
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return AlertDialog.Builder(requireActivity())
+                .setTitle(R.string.app_name)
+                .setMessage(R.string.author)
+                .setPositiveButton(android.R.string.ok,null)
+                .create()
+    }
 }
